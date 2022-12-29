06015c00b37084e514a987fd91aef8587f32ee6c
Siraj Samsudeen <mailsiraj@gmail.com>
2022-12-29 13:28:07 +0530

    freelancer-rates - dayRate function - handles decimal values for hourlyRate

--- freelancer-rates.spec.js
+++ freelancer-rates.spec.js
@@ -5,5 +5,11 @@ describe('freelancer-rates', () => {
       expect(dayRate(1)).toBe(8);
       expect(dayRate(2)).toBe(2 * 8);
     });
+    it('handles decimal values', () => {
+      const ROUNDING_PRECISION = 6;
+      //   expect(dayRate(97.654321)).toBe(97.654321 * 8);
+      // floating point arithmetic is flaky - hence better to avoid toBe
+      expect(dayRate(97.654321)).toBeCloseTo(97.654321 * 8, ROUNDING_PRECISION);
+    });
   });
 });
