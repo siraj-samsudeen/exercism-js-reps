ee995c7a551d94cab1c6f9bbb1763a63be26e69a
Siraj Samsudeen <mailsiraj@gmail.com>
2022-12-29 13:15:24 +0530

    freelancer-rates - refactor to add a describe block for dayrate

--- freelancer-rates.spec.js
+++ freelancer-rates.spec.js
@@ -1,7 +1,9 @@
 import { dayRate } from './freelancer-rates';
 describe('freelancer-rates', () => {
-  it('dayRate calculates given hourly rate, assuming 8-hour day', () => {
-    expect(dayRate(1)).toBe(8);
-    expect(dayRate(2)).toBe(2 * 8);
+  describe('dayRate', () => {
+    it('calculates given hourly rate, assuming 8-hour day', () => {
+      expect(dayRate(1)).toBe(8);
+      expect(dayRate(2)).toBe(2 * 8);
+    });
   });
 });
