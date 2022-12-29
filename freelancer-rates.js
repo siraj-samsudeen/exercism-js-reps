396c78cbf4936b0584f7011916504f1934171258
Siraj Samsudeen <mailsiraj@gmail.com>
2022-12-29 13:00:06 +0530

    freelancer-rates - dayRate function - add triangulation test

--- freelancer-rates.js
+++ freelancer-rates.js
@@ -1,3 +1,4 @@
-export function dayRate() {
-  return 8;
+export function dayRate(hourlyRate) {
+  const HOURS_PER_DAY = 8;
+  return hourlyRate * HOURS_PER_DAY;
 }
--- freelancer-rates.spec.js
+++ freelancer-rates.spec.js
@@ -2,5 +2,6 @@ import { dayRate } from './freelancer-rates';
 describe('freelancer-rates', () => {
   it('dayRate calculates given hourly rate, assuming 8-hour day', () => {
     expect(dayRate(1)).toBe(8);
+    expect(dayRate(2)).toBe(2 * 8);
   });
 });
