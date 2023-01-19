2cd2b982e8c2370d5bf6f0eea9b5403588065c97
Siraj Samsudeen <mailsiraj@gmail.com>
2023-01-19 11:19:41 +0530

    removing all implementation and spec files to bring the exercism version for all

--- freelancer-rates.js
+++
@@ -1,4 +0,0 @@
-export function dayRate(hourlyRate) {
-  const HOURS_PER_DAY = 8;
-  return hourlyRate * HOURS_PER_DAY;
-}
--- hello-world.spec.js
+++
@@ -1,6 +0,0 @@
-import { helloWorld } from './hello-world';
-describe('hello world', () => {
-  it('returns Hello world!', () => {
-    expect(helloWorld()).toEqual('Hello world!');
-  });
-});
