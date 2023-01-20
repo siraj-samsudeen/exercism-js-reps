f527379920be99bae990f882454d7c7ae2501704
thiyagu23 <trendythiyagu1523@gmail.com>
2023-01-20 18:03:43 +0530

    door-policy

--- door-policy.js
+++ door-policy.js
@@ -6,9 +6,8 @@ export function frontDoorPassword(word){
    return word[0].toUpperCase() + word.slice(1).toLowerCase()
 }
 
-export function backDoorResponse(word){
-   const lastLetter =  word.trim()  
-    return lastLetter.slice(-1)
+export function backDoorResponse(word)  {
+    return word.trim().slice(-1)
 }
 
 export function backDoorPassword(word){
